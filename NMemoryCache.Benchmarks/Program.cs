3a97ca974873d9f0234224a8832b311a8abed90d
PanyushkinD <PanyushkinD@quantumart.ru>
2017-08-24 10:12:12 +0300

    Move cache implementation from CSharp.Common

---
+++ NMemoryCache.Benchmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NMemoryCache.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.WriteLine($"NMemoryCache.Benchmarks.exe keys:int tags:int tagsPerKey:int lifetimeSec invalidationMsec");
+                return;
+            }
+
+            int keys = Int32.Parse(args[0]);
+            int tags = Int32.Parse(args[1]);
+            int tagsPerKey = Int32.Parse(args[2]);
+            int lifetimeSec = Int32.Parse(args[3]);
+            int invalidationMsec = Int32.Parse(args[4]);
+
+            new CacheBenchmark().Run(keys, tags, tagsPerKey, lifetimeSec, invalidationMsec);
+        }
+    }
+}
---
+++ NMemoryCache/ConcurrentTags/TagEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace NMemoryCache.ConcurrentTags
+{
+    internal class TagEntry : ConcurrentDictionary<CacheEntry, object>
+    {
+        private bool _isRemoved;
+        
+        /// <summary>
+        /// Create already not empty <see cref="TagEntry"/>.
+        /// </summary>
+        public TagEntry(CacheEntry cacheEntry, object key)
+        {
+            _isRemoved = false;
+            
+            TryAdd(cacheEntry, key);
+        }
+        
+        public bool IsRemoved => Volatile.Read(ref _isRemoved);
+
+        public void MarkAsRemoved()
+        {
+            Volatile.Write(ref _isRemoved, true);
+        }
+
+        public bool IsActive => !IsRemoved;
+    }
+}
